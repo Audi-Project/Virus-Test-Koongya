e84eb0dbb1b62c97651ca76642801caa9a26acdb
Ryu jisu <wltn1418@gmail.com>
2023-05-28 00:27:01 +0900

    test

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <>
       <h1 className="text-red-500">how to use Tailwind</h1>
+      <p>djfkdjfkdf</p>
       <H>how to use Tailwind styled-compoenents</H>
       <p>아래 링크에서 vscode setting 하셔야합니다</p>
       <a href="https://www.npmjs.com/package/tailwind-styled-components">
