736a1b651a536f46f17d7d438b61896c1ad0fce2
Ryu jisu <wltn1418@gmail.com>
2023-05-31 23:15:36 +0900

    animation

--- src/pages/Loading.jsx
+++ src/pages/Loading.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Container } from '../styles/style';
-import loadingVirus from '../assets/loadingVirus.svg';
+import LoadingVirus from '../assets/loadingVirus.png';
 import tw from 'tailwind-styled-components';
+import 'animate.css';
+import styled, { keyframes } from 'styled-components';
 
 const Loading = () => {
   const navigation = useNavigate();
@@ -16,21 +18,40 @@ const Loading = () => {
   return (
     <LoadingContainer>
       <LoadingP>진단서를 작성하고 있습니다</LoadingP>
-      <img src={loadingVirus} alt="로딩중" />
-      <div className="flex justify-center items-center">
-        <LoadingP>나의 바이러스 유형 분석 중</LoadingP>
-        <LoadingP>...</LoadingP>
+      <CloudDiv>
+        <img src={LoadingVirus} alt="로딩중" />
+      </CloudDiv>
+      <div className="flex flex-col justify-center items-center">
+        <LoadingP>나의 바이러스 유형</LoadingP>
+        <LoadingP>분석 중...</LoadingP>
       </div>
     </LoadingContainer>
   );
 };
 
 const LoadingContainer = tw(Container)`
-justify-center
+justify-center items-center gap-4
 `;
 
 const LoadingP = tw.p`
 text-loadingFont text-[22px]
+`;
+
+const CloudDiv = styled.div`
+  animation: motion 1s linear infinite alternate;
+  margin-top: 0;
+
+  @keyframes motion {
+    0% {
+      transform: translateY(15px);
+    }
+    50% {
+      transform: translateY(0px);
+    }
+    100% {
+      transform: translateY(-15px);
+    }
+  }
 `;
 
 export default Loading;
