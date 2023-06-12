64d14c7a9fc6f26070a9291b05de96bced81f35a
Ryu jisu <wltn1418@gmail.com>
2023-06-12 13:06:57 +0900

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,31 @@ export default {
         mainPurple: '#8152F1',
         loadingFont: '#5B3037',
       },
+      keyframes: {
+        typing: {
+          from: {
+            width: '0px',
+          },
+          to: {
+            width: '18px',
+          },
+        },
+        motion: {
+          '0%': {
+            transform: 'translateY(15px)',
+          },
+          '50%': {
+            transform: 'translateY(0px)',
+          },
+          '100%': {
+            transform: 'translateY(-15px)',
+          },
+        },
+      },
+      animation: {
+        typing: 'typing 1.5s steps(20) infinite',
+        motion: 'motion 0.8s linear infinite alternate',
+      },
       boxShadow: {
         '3xl': '2px 2px 0px rgba(128, 89, 230, 0.2)',
       },
